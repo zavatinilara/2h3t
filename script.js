50a765e8ac3b841514a63ca40ad8f5c82930c34e
zavatinilara <127403856+zavatinilara@users.noreply.github.com>
2023-09-15 08:08:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-clicado=() => alert("alo mundo")
+clicado=() => alert("Alo mundo");
+mensagem=(msg) => console.log(msg);
+soma = (a,b) => a + b;
+sub= (a,b)  => a - b;
+mult= (a,b) => a * b;
+div= (a,b) => a / b;
+juros_simples = (c,j) => mult(c,div(j,100));
+montante_juros_simples = (c,j) => soma(juros_simples(c,j),c);
+
+mensagem (soma(4,soma(7,soma(11,15))));
+mensagem (soma(15,soma(11,soma(4,7))));
+mensagem (sub(18,7));
+mensagem (soma(2,div(2,2)));
+mensagem (juros_simples(420,18))
